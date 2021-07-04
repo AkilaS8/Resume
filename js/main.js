a40a654ca9775ba191f80505d7c1b99bbb90f405
Akila Ishan <59306630+AkilaS8@users.noreply.github.com>
2021-07-05 02:36:36 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,148 @@
+(function ($) {
+    "use strict";
+
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#loader').length > 0) {
+                $('#loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+
+    // Typed Initiate
+    if ($('.hero .hero-text h2').length == 1) {
+        var typed_strings = $('.hero .hero-text .typed-text').text();
+        var typed = new Typed('.hero .hero-text h2', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+
+    // Skills
+    $('.skills').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            }
+        }
+    });
+
+
+
+    // Portfolio filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-filter li').on('click', function () {
+        $("#portfolio-filter li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+})(jQuery);
+
+function submitForm(){
+
+  let name = name_form.value;
+  let email = email_form.value;
+  let subject = subject_form.value;
+  let message = message_form.value;
+
+  if(!name_form.value){
+    alert("Please Enter Your Name");
+  }else if(!email_form.value){
+    alert("Please Enter Your Email");
+  }else if(!validateEmail(email)){
+    alert("Please Enter Valued Email");
+  }else if(!subject_form.value){
+    alert("Please Enter Subject to message");
+  }else if(!message_form.value){
+    alert("Please Enter your Message");
+  }else{
+    sendEmail(name, email, subject, message);
+  }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+ function sendEmail(name, email, subject, message){
+  Email.send({
+     Host: "smtp.gmail.com",
+     Username: "solutionsai20@gmail.com",
+     Password: "zmltkcelabavqjli",
+     To: "ishanakilas8@gmail.com",
+     From: "solutionsai20@gmail.com",
+     Subject: 'Profile Mail: ' + subject + '',
+     Body: 'Name: '+ name +' <br/>Email: '+ email + ' <br/>Message:  '+ message +'',
+   }).then(alert("Message sent Successfully. Thank you for contact me!"));
+ }
